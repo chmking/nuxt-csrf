64c2e343996e5d5ea07e5b8bd31c491ef05a310a
Christopher King <chris@coyfox.net>
2024-10-25 11:32:39 -0600

    Updated dependencies for audit

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -2,10 +2,13 @@ import CsrfModule from '..'
 
 export default defineNuxtConfig({
   modules: [CsrfModule],
+
   csrf: {
     cookie: {
       name: 'foo',
       sameSite: 'lax',
     },
   },
+
+  compatibilityDate: '2024-09-19',
 })
